b9fbc00b39b71c3ed305c95c4022c475b2f61a2c
krayers <49661556+krayers@users.noreply.github.com>
2019-09-25 15:18:05 +0200

    Update gestion_n_s.sql

--- site_final/gestion_n_s.sql
+++ site_final/gestion_n_s.sql
@@ -1,32 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 4.8.3
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1:3306
--- Généré le :  mer. 25 sep. 2019 à 12:59
--- Version du serveur :  5.7.23
--- Version de PHP :  7.2.10
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de données :  `gestion_n_s`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `admin`
---
+DROP DATABASE IF EXISTS GESTION_N_S;
+CREATE DATABASE GESTION_N_S;
+use GESTION_N_S;
 
 DROP TABLE IF EXISTS `admin`;
 CREATE TABLE IF NOT EXISTS `admin` (
